a64d3d2a40368098be6aa437fa9f9fed5126a1a5
leomuko <kezzieleo@gmail.com>
2020-09-17 12:07:48 +0300

    Create project UIs

---
+++ app/src/main/java/com/example/sacco/SavingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.sacco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings);
+    }
+}
